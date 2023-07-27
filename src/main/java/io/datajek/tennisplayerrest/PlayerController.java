56df84e17e095dad02f65f0ef799ebea43ee54ae
Ki Woo Kim <kwedgemusic@gmail.com>
2023-07-27 15:09:40 -0700

    get player by id'

--- src/main/java/io/datajek/tennisplayerrest/PlayerController.java
+++ src/main/java/io/datajek/tennisplayerrest/PlayerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,4 +22,8 @@ public class PlayerController {
     return service.getAllPlayers();
   }
 
+  @GetMapping("/players/{id}")
+  public Player getPlayer(@PathVariable int id) {
+    return service.getPlayer(id);
+  }
 }
--- src/main/java/io/datajek/tennisplayerrest/PlayerService.java
+++ src/main/java/io/datajek/tennisplayerrest/PlayerService.java
@@ -1,6 +1,7 @@
 package io.datajek.tennisplayerrest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,5 +14,23 @@ public class PlayerService {
   //getAllPlayers
   public List<Player> getAllPlayers(){
     return repo.findAll();
+  }
+
+  //getPlayer By ID
+  public Player getPlayer(int id){
+    // So, in this approach of coding, we used Optional and avoided explicitly checking for null values.
+    Optional<Player> tempPlayer = repo.findById(id);
+
+    Player p = null;
+
+    //if the Optional has a value, assign it to p
+    if(tempPlayer.isPresent())
+        p = tempPlayer.get();
+
+    //if value is not found, throw a runtime exception
+    else
+        throw new RuntimeException("Player with id "+ id + " not found.");
+
+    return p;
   }
 }
